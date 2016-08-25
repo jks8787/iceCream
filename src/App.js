5368b164818a3d3b8ab1a1be3742230e18a543f4
Janice Smith <janice.k.n.smith@gmail.com>
2016-08-25 17:23:25 -0400

    now has a static recharts pie chart

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import VictoryPieChart from './components/VictoryPieChart.js';
 import RechartsLineChart from './components/RechartsLineChart';
 import RechartsBarChart from './components/RechartsBarChart';
+import RechartsPieChart from './components/RechartsPieChart';
 
 const App = () => (
   <div>
+    <RechartsPieChart />
     <RechartsLineChart />
     <RechartsBarChart />
     <VictoryPieChart />
